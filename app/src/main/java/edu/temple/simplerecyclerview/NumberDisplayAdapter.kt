d7e2815556c7bd832428adc118c77287974e192d
TJG62 <43542691+TJG62@users.noreply.github.com>
2025-02-11 12:11:19 -0500

    Completed the logic to populate the recycle view per instructions in class.

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,14 +1,37 @@
 package edu.temple.simplerecyclerview
 
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 // Step 3: Complete adapter implementation
 
-class NumberDisplayAdapter /* TODO Step 3a: Provide Adapter Parent */ {
+class NumberDisplayAdapter(private val _numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-    class NumberViewHolder (val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
-    // TODO Step 3b: Complete function definitions for adapter
+    class NumberViewHolder (textView: TextView) : RecyclerView.ViewHolder (textView)
+    {
+        val numberView = textView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        return NumberViewHolder(
+            TextView(
+                parent.context
+            ).apply {
+                setPadding(10,10,10,10)
+            }
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return _numbers.size
+    }
+
+    override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
+       holder.numberView.text =_numbers[position].toString()
+        holder.numberView.textSize = _numbers[position].toFloat()
+    }
 
 }
